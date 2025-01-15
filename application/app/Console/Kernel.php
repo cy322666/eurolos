f536d547d670a7e0b11f156a28612542b8b93684
cy322666 <cy322666@gmail.com>
2025-01-15 09:47:35 +0200

    add commands

--- application/app/Console/Kernel.php
+++ application/app/Console/Kernel.php
@@ -12,7 +12,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+         $schedule->command('app:get-entities')->dailyAt('07:00');
+         $schedule->command('app:get-lead-create')->hourly();
+         $schedule->command('app:get-calls')->hourly();
     }
 
     /**
