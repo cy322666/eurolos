1fff907614fbafc420ebb42a72766a555a554447
cy322666 <cy322666@gmail.com>
2025-01-21 10:22:28 +0200

    build

--- application/app/Jobs/Events/TalkJob.php
+++ application/app/Jobs/Events/TalkJob.php
@@ -28,17 +28,17 @@ class TalkJob implements ShouldQueue
     public function handle(Request $request): void
     {
         Talk::query()->create([
-            'talk_id' => $request->talk_id,
-            'talk_created_at' => Carbon::parse($request->created_at)->format('Y-m-d H:i:s'),
-            'rate' => $request->rate,
-            'contact_id' => (int)$request->contact_id,
-            'chat_id' => $request->chat_id,
-            'entity_id' => (int)$request->entity_id,
-            'entity_type' => $request->entity_type,
-            'is_in_work' => $request->is_in_work,
-            'is_read' => $request->is_read,
-            'origin' => $request->origin,
-            'body' => $request->json(),
+            'talk_id' => $request->talk['add'][0]['talk_id'],
+            'talk_created_at' => Carbon::parse($request->talk['add'][0]['created_at'])->format('Y-m-d H:i:s'),
+            'rate' => $request->talk['add'][0]['rate'],
+            'contact_id' => (int)$request->talk['add'][0]['contact_id'],
+            'chat_id' => $request->talk['add'][0]['chat_id'],
+            'entity_id' => (int)$request->talk['add'][0]['entity_id'],
+            'entity_type' => $request->talk['add'][0]['entity_type'],
+            'is_in_work' => $request->talk['add'][0]['is_in_work'],
+            'is_read' => $request->talk['add'][0]['is_read'],
+            'origin' => $request->talk['add'][0]['origin'],
+            'body' => json_encode($request->talk['add'][0]),
         ]);
     }
 }
