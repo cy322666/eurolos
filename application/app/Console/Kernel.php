ea236db2690d74c16aa66494b93058a41becce14
cy322666 <cy322666@gmail.com>
2025-02-10 14:53:16 +0200

    build

--- application/app/Console/Kernel.php
+++ application/app/Console/Kernel.php
@@ -13,10 +13,11 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
          $schedule->command('app:get-entities')->dailyAt('07:00');
-         $schedule->command('app:get-lead-create')->dailyAt('04:00');
-         $schedule->command('app:get-leads')->dailyAt('05:00');
-         $schedule->command('app:get-lead-statuses')->dailyAt('06:00');
-         $schedule->command('app:get-calls')->dailyAt('08:00');
+         $schedule->command('app:get-lead-create')->dailyAt('01:00');
+         $schedule->command('app:get-lead-statuses')->dailyAt('02:00');
+         $schedule->command('app:get-updated-leads')->dailyAt('03:00');
+         $schedule->command('app:get-leads')->dailyAt('04:00');
+         $schedule->command('app:get-calls')->dailyAt('05:00');
 
          $schedule->command('telescope:prune --hours=72')->daily();
     }
