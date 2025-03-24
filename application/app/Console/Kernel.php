dff30a4f0fba0271d27a829291500ac022a844af
cy322666 <cy322666@gmail.com>
2025-03-24 10:45:29 +0200

    build

--- application/app/Console/Kernel.php
+++ application/app/Console/Kernel.php
@@ -15,22 +15,22 @@ class Kernel extends ConsoleKernel
         //9 - 9.30
         //
         $schedule->command('app:get-entities')->dailyAt('08:00');
-        $schedule->command('app:get-lead-create')->dailyAt('08:00');
         $schedule->command('app:get-lead-statuses')->dailyAt('08:01');
         $schedule->command('app:get-updated-leads')->dailyAt('08:02');
         $schedule->command('app:get-calls')->dailyAt('08:03');
 
+        $schedule->command('app:get-lead-create')->dailyAt('07:00');
         $schedule->command('app:get-leads 2000')->dailyAt('07:05');
         $schedule->command('app:get-leads 2000')->dailyAt('07:15');
         $schedule->command('app:get-leads 2000')->dailyAt('07:25');
         $schedule->command('app:get-leads 7000')->dailyAt('07:35');
 
-        $schedule->command('app:get-entities')->dailyAt('16:00');
-        $schedule->command('app:get-lead-create')->dailyAt('16:00');
-        $schedule->command('app:get-lead-statuses')->dailyAt('16:01');
-        $schedule->command('app:get-updated-leads')->dailyAt('16:02');
-        $schedule->command('app:get-calls')->dailyAt('16:03');
+        $schedule->command('app:get-entities')->dailyAt('17:00');
+        $schedule->command('app:get-lead-statuses')->dailyAt('17:01');
+        $schedule->command('app:get-updated-leads')->dailyAt('17:02');
+        $schedule->command('app:get-calls')->dailyAt('17:03');
 
+        $schedule->command('app:get-lead-create')->dailyAt('16:00');
         $schedule->command('app:get-leads 10000')->dailyAt('16:05');
         $schedule->command('app:get-leads 2000')->dailyAt('16:05');
         $schedule->command('app:get-leads 2000')->dailyAt('16:15');
